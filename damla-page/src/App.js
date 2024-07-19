6ed64f8fd186e390de101bd4eb339ea5ad187e5a
Slavi Dimitrov <slavi@orbisds.com>
2024-07-20 00:44:32 +0300

    Initialise new React project and add the Header and Footer components

---
+++ damla-page/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// App.jsx
+function App() {
+  return (
+    <>
+      <Header />
+      {/* Body */}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ damla-page/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <nav className="h-20 flex justify-evenly bg-slate-600">
+      <h1 className="h-10 text-center mt-5 text-3xl font-bold underline text-white">
+        Damla's Page
+      </h1>
+      <ul className="w-80 h-10 mt-8 flex justify-around">
+        <li className="hover:color-red">
+          <a href="#porfolio">Портфолио</a>
+        </li>
+        <li>
+          <a href="#porfolio">Умения</a>
+        </li>
+        <li>
+          <a href="#porfolio">Творби</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
