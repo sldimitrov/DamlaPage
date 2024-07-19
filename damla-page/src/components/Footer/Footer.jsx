a81631e903232274fd6e40f963bf4e481486de82
Slavi Dimitrov <slavi@orbisds.com>
2024-07-20 02:08:58 +0300

    add footer to the page

--- damla-page/src/components/Footer/Footer.jsx
+++ damla-page/src/components/Footer/Footer.jsx
@@ -1,3 +1,143 @@
 export default function Footer() {
-  return 1;
+  const customStyle = {
+    width: `23px`,
+    height: `23px`, // Similarly, set the height dynamically
+  };
+
+  return (
+    <footer className="bg-dark text-white pt-5 pb-4">
+      <div className="container text-center text-md-left ">
+        <div className="row text-center text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h3 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Damla's Page
+            </h3>
+            <p>
+              Дамла Сезгин Кемик е родена на 2 април 2005 година, в град Айтос.
+              От ранна детска възраст проявява интерес към изкуството.лауреат на
+              различни конкурси в България и по целия свят в сферата на
+              литературата и изкуството. Освен с литературните си постижения
+              Дамла набра популярност и със своя макет на Айфеловата кула.
+              Новината е отразена по БНТ, БТВ, Нова ТВ, МОН и др.
+            </p>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h3 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Награди
+            </h3>
+            <p>
+              <a href="#" className="text-white">
+                „Янаки Петров“
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                „Изящното перо“
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                „Георги Тодоров“
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                „Млад разказвач“
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                „Иван Бързаков“
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-gray-400">
+                и още много други
+              </a>
+            </p>
+          </div>
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Полезни линкове
+            </h5>
+            <p>
+              <a href="#" className="text-white">
+                Профил
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                Присъедини се
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                Писателска дейност
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white">
+                Помощ
+              </a>
+            </p>
+          </div>
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Contact
+            </h5>
+            <p>
+              <i className="fas fa-home mr-3">Chernograd, Bourgas, 23, BG</i>
+            </p>
+            <p>
+              <i className="fas fa-envelope mr-3">damla.s.k@abv.bg</i>
+            </p>
+            <p>
+              <i className="fas fa-phone mr-3">+359 88 6478698</i>
+            </p>
+          </div>
+        </div>
+        <hr className="mb-4" />
+        <div className="rob align-items-center">
+          <div className="col-md-7 col-lg-8">
+            <p>
+              Copyright 2020 All rights reserved by:
+              <a href="#">
+                <strong className="text-warning">Slavi Dimitrov</strong>
+              </a>
+            </p>
+          </div>
+          <div className="col-md-5 col-lg-4">
+            <div className="text-center text-md-right">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={customStyle}
+                  >
+                    <i className="fab fa-facebook"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#" className="btn-floating btn-sm text-white">
+                    <i className="fab fa-google-plus"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#" className="btn-floating btn-sm text-white">
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#" className="btn-floating btn-sm text-white">
+                    <i className="fab fa-youtube"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- damla-page/src/components/Header/Header.jsx
+++ damla-page/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <nav className="h-20 flex justify-evenly bg-slate-600">
+    <nav className="h-20 flex justify-evenly bg-blue-600">
       <h1 className="h-10 text-center mt-5 text-3xl font-bold underline text-white">
         Damla's Page
       </h1>
       <ul className="w-80 h-10 mt-8 flex justify-around">
-        <li className="hover:color-red">
+        <li className="color:">
           <a href="#porfolio">Портфолио</a>
         </li>
         <li>
