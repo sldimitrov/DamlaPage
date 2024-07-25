c0c307edb490161bf85697c88ca92b2da67408a9
Slavi Dimitrov <slavi@orbisds.com>
2024-07-26 00:02:06 +0300

    add fonts, details and other changes in the tailwind css

--- src/components/Opinions/View/View.jsx
+++ src/components/Opinions/View/View.jsx
@@ -18,11 +18,13 @@ export default function View({ photo, name, info, opinion, index }) {
       )}
 
       <section className="flex flex-col justify-center m-3">
-        <h2 className="text-black text-center text-3xl md:text-4xl">{name}</h2>
-        <h3 className="text-black text-center text-base md:text-lg mt-3 text-slate-300 italic">
+        <h2 className="text-black font-serif text- text-center text-3xl md:text-4xl">
+          {name}
+        </h2>
+        <h3 className=" text-center font-normal text-base md:text-lg mt-3 text-slate-500 italic">
           {info}
         </h3>
-        <p className="text-black text-base md:text-xl text-center mt-3">
+        <p className="text-black font-sans text-base md:text-xl text-center mt-3">
           {opinion}
         </p>
       </section>
